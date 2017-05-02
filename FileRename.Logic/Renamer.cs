2c9236b68d51cd21b81669b23f91db8bdc1da4b7
Carlos Garcia <Csharles@users.noreply.github.com>
2017-05-02 12:58:08 -0600

    Added methods to colect file names

--- FileRename.Logic/Renamer.cs
+++ FileRename.Logic/Renamer.cs
@@ -9,11 +9,25 @@ namespace FileRename.Logic
     public class Renamer
     {
         public List<string> FileNames { get; set; }
+        public string baseAdress { get; set; }
         public List<string> NewNames { get; private set; }
         public List<string> listFiles(string folderAddress)
         {
             var fileNames = new List<string>();
-            
+            return fileNames;           
+        }
+        private List<string> renameFiles(List<string>fileNames, string nameTemplate)
+        {
+            var newNames = new List<string>();
+            foreach (var fileName in fileNames)
+            {
+
+            }
+            return newNames;
+        }
+        public Renamer (string baseAddress)
+        {
+            this.baseAdress = baseAdress;
         }
     }
 }
